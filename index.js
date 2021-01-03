dd18d37f812642c731619d66cea352d0d03e1c8d
L D <ldeunert@gmail.com>
2021-01-03 23:55:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments){
   var newArray = []
   for(let i=0; i< musicians.length; i++){
-    newArray.push(`"${musicians[i]} plays ${instruments[i]}"`)
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return newArray
 }
