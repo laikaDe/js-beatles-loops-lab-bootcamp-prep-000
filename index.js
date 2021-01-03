ea216849a8ecb02b03a88dd456e54a4b2f37c3bd
L D <ldeunert@gmail.com>
2021-01-03 23:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  newArray = []
+  for(let i=0; i< musicans.length; i++){
+    newArray.push(`"${musicians[i]} plays ${instruments[i]}"`)
+  }
+  return newArray
+}
